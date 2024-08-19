a38b4ac721dc85a539a2eddd568950571eba53af
harishraajaa <rvharishraajaa@gmail.com>
2024-08-19 19:50:21 +0530

    Port config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import AppRoutes from './src/routes/index.routes.js'
 
+const PORT = process.env.PORT || 8000
+
 const app = express()
 
 app.use(express.json())
 app.use(AppRoutes)
 
-app.listen('8000',()=>console.log("App server is running in 8000"))
+app.listen(PORT,()=>console.log(`App server is running in ${PORT}`))
