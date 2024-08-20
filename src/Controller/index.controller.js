4755625e91dfa359d3d3fd80dc93be3d260c96aa
harishraajaa <rvharishraajaa@gmail.com>
2024-08-20 22:23:04 +0530

    HomePage Update

--- src/Controller/index.controller.js
+++ src/Controller/index.controller.js
@@ -1,13 +1,13 @@
 const homePage = (req, res) => {
     try {
       res.status(200).send(`
-          <div style="padding:10px;"><h1 style = "text-align:center">Hall Booking API</h1>
-          <ul><li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/get-all-room - To GET All Room Details.</h3></li>
-          <li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/create-room - To POST the Room Details in the API.</h3></li>
-          <li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/booking-room - To POST the booked Room Details in the API.</h3></li>
-          <li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/booked-data - To GET all rooms with Booked Details.</h3></li>
-          <li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/customer-data - To GET all Customer with Booked Details</h3></li>
-          <li><h3>https://hall-booking-mcgh.onrender.com/hall-booking/bookedCount/:customer_name - To GET Customer Details with number of times the room booked.</h3></li>
+          <div style="padding:10px;font-family: Arial"><h1 style = "text-align:center">Hall Booking API</h1>
+          <ul><li><h3>https://hall-booking-harishcorp.onrender.com/hall/getAllRoom - To GET All Room Details.</h3></li>
+          <li><h3>https://hall-booking-harishcorp.onrender.com/hall/createRoom - To POST the Room Details in the API.</h3></li>
+          <li><h3>https://hall-booking-harishcorp.onrender.com/hall/bookRoom - To POST the booked Room Details in the API.</h3></li>
+          <li><h3>https://hall-booking-harishcorp.onrender.com/hall/getBookedData - To GET all rooms with Booked Details.</h3></li>
+          <li><h3>https://hall-booking-harishcorp.onrender.com/hall/getCustData - To GET all Customer with Booked Details</h3></li>
+          <li><h3>https://hall-booking-harishcorp.onrender.com/hall/getCustCount/:customer_name - To GET Customer Details with number of times the room booked.</h3></li>
           </ul></div>
           `);
     } catch (error) {
